5685864b3ef9cb308e967a584e383c9800887a83
k8kiw <15900197156@163.com>
2019-04-10 19:55:54 +0800

    homework
    加入.gitignore防止移除不必要文件，提交四棱锥的作业

---
+++ 四棱锥/Rectangular Pyramid/Pyramid_VS.hlsl
@@ -0,0 +1,13 @@
+#include "Pyramid.hlsli"
+
+VertexOut VS( VertexIn vIn )
+{
+	VertexOut vOut;
+
+	vOut.posH = mul(float4(vIn.posL, 1.0f), g_World);
+	vOut.posH = mul(vOut.posH, g_View);
+	vOut.posH = mul(vOut.posH, g_Proj);
+	vOut.color = vIn.color;
+
+	return vOut;
+}
